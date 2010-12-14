57dadc0fb47e440d5dceb3f4b50b112102ac6682
bjtusq <bjtusq@e71795da-d208-e133-f019-3e19836ab347>
2010-12-14 03:04:20 +0000

    add

---
+++ trunk/JavaTest/JavaTest/src/appContext/GetFileDir.java
@@ -0,0 +1,19 @@
+package appContext;
+
+public class GetFileDir {
+
+	private static String str;
+	
+	public GetFileDir(){
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+}
